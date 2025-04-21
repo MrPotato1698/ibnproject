1d8cb9bb9b055723a3e36f1d3ca697f5fe3df084
Manuel J. Gonzalez <76817865+MrPotato1698@users.noreply.github.com>
2025-04-21 09:24:19 +0200

    Agregar archivos de configuración y estructura inicial del proyecto Spring Boot

---
+++ src/main/java/com/ibn/ibnproject/IbnprojectApplication.java
@@ -0,0 +1,13 @@
+package com.ibn.ibnproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbnprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbnprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibn/ibnproject/IbnprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibn.ibnproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbnprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
